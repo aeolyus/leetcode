9bdf0e594a084d9aab2226db28c29751cd5084e0
Richard Huang <richardhuang.huang@gmail.com>
2019-01-14 22:29:00 -0800

    sum of two ints

---
+++ src/SumOfTwoIntegers.java
@@ -0,0 +1,10 @@
+public class SumOfTwoIntegers {
+    public int getSum(int a, int b) {
+        while (b != 0) {
+            int c = (a & b) << 1;
+            a ^= b;
+            b = c;
+        }
+        return a;
+    }
+}
