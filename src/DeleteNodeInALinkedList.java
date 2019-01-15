d3a7427bf3db900813803adaf0aadb4c35efa426
Richard Huang <richardhuang.huang@gmail.com>
2019-01-14 23:08:02 -0800

    delete node in a linked list

---
+++ src/DeleteNodeInALinkedList.java
@@ -0,0 +1,14 @@
+public class DeleteNodeInALinkedList {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+      public class ListNode {
+          int val;
+          ListNode next;
+          ListNode(int x) {
+              val = x;
+          }
+      }
+}
