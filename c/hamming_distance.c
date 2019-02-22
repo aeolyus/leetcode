2f07b30ec0bf01fcd806ea75b5a3786901761149
Richard Huang <richardhuang.huang@gmail.com>
2019-02-21 17:30:17 -0800

    hamming distance modified

--- c/hamming_distance.c
+++ c/hamming_distance.c
@@ -1,7 +1,7 @@
 int hammingDistance (int x, int y) {
     int num = x ^ y, c = 0;
     while (num > 0) {
-        if ((num >> 1)<< 1 != num) {
+        if (num & 1) {
             c++;
         }
         num >>= 1;
