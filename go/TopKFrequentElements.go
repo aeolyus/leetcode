902a8f33f357a10882c6ce1ff27d6a8403521a81
Richard Huang <richardhuang.huang@gmail.com>
2019-07-14 21:00:35 -0700

    top k frequent elements

---
+++ go/TopKFrequentElements.go
@@ -0,0 +1,61 @@
+package main
+
+import "container/heap"
+
+type Item struct {
+	Key      int
+	priority int
+}
+
+type PriorityQueue []Item
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	return pq[i].priority > pq[j].priority
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
+
+func (pq *PriorityQueue) Push(x interface{}) {
+	n := len(*pq)
+	item := x.(*Item)
+	item.index = n
+	*pq = append(*pq, item)
+}
+
+func (pq *PriorityQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	item.index = -1 // for safety
+	*pq = old[0 : n-1]
+	return item
+}
+
+// update modifies the priority and value of an Item in the queue.
+func (pq *PriorityQueue) update(item *Item, value string, priority int) {
+	item.value = value
+	item.priority = priority
+	heap.Fix(pq, item.index)
+}
+
+func topKFrequent(nums []int, k int) []int {
+	h := make(PriorityQueue, 0)
+}
+
+func top(nums []int) map[int]int {
+	r := make(map[int]int)
+	for _, v := range nums {
+		if _, ok := r[v]; !ok {
+			r[v] = 1
+		} else {
+			r[v]++
+		}
+	}
+	return r
+}
