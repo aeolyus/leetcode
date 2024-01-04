4e12432b7684625806380184601f39acc3881993
Richard Huang <richardhuang.huang@gmail.com>
2024-01-03 22:55:16 -0800

    [0088][python] merge sorted array

---
+++ python/88/main.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+    def merge(
+            self,
+            nums1: List[int],
+            m: int,
+            nums2: List[int],
+            n: int
+    ) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        a = m - 1
+        b = n - 1
+        i = m + n - 1
+        while b >= 0:
+            if a >= 0 and nums1[a] > nums2[b]:
+                nums1[i] = nums1[a]
+                a -= 1
+            else:
+                nums1[i] = nums2[b]
+                b -= 1
+            i -= 1
