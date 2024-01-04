09229d98d1cf240c5c3cddf4810c7dc75534c27b
Richard Huang <richardhuang.huang@gmail.com>
2024-01-03 23:38:58 -0800

    [0088][python] add tests

---
+++ python/0088/test.py
@@ -0,0 +1,21 @@
+import unittest
+from main import Solution
+
+
+class Test00088(unittest.TestCase):
+    def test(self):
+        testcases = [
+            [[1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3, [1, 2, 2, 3, 5, 6]],
+            [[1], 1, [], 0, [1]],
+            [[0], 0, [1], 1, [1]],
+        ]
+
+        for i in testcases:
+            nums1, m, nums2, n, expected = i
+            s = Solution()
+            s.merge(nums1, m, nums2, n)
+            self.assertEqual(expected, nums1)
+
+
+if __name__ == '__main__':
+    unittest.main()
