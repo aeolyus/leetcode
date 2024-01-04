ddae23ae4729e53b4002db93b5acae079b58f270
Richard Huang <richardhuang.huang@gmail.com>
2024-01-03 23:44:45 -0800

    [0026][python] Add tests

---
+++ python/0026/test.py
@@ -0,0 +1,20 @@
+import unittest
+from main import Solution
+
+
+class Test00026(unittest.TestCase):
+    def test(self):
+        testcases = [
+            [[1, 1, 2], [1, 2]],
+            [[0, 0, 1, 1, 1, 2, 2, 3, 3, 4], [0, 1, 2, 3, 4]],
+        ]
+
+        for i in testcases:
+            nums, expected = i
+            s = Solution()
+            k = s.removeDuplicates(nums)
+            self.assertEqual(expected, nums[:k])
+
+
+if __name__ == '__main__':
+    unittest.main()
