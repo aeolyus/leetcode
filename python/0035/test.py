fcb8fb87320eb07ced813030252939f72a96f52b
Richard Huang <richardhuang.huang@gmail.com>
2024-01-09 21:55:15 -0800

    [0035][python] search insert position

---
+++ python/0035/test.py
@@ -0,0 +1,21 @@
+import unittest
+from main import Solution
+
+
+class Test0001(unittest.TestCase):
+    def test(self):
+        testcases = [
+            [[1, 3, 5, 6], 5, 2],
+            [[1, 3, 5, 6], 2, 1],
+            [[1, 3, 5, 6], 7, 4],
+        ]
+
+        for i in testcases:
+            nums, target, expected = i
+            s = Solution()
+            actual = s.searchInsert(nums, target)
+            self.assertEqual(expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
