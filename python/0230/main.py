d6b6f6f053c262ea0ea4faf74c80b5435a240094
Richard Huang <richardhuang.huang@gmail.com>
2024-01-20 20:24:40 -0800

    [0230][python] kth smallest element in a bst

---
+++ python/0230/main.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        curr = root
+        stack = []
+        while curr or stack:
+            if curr:
+                stack.append(curr)
+                curr = curr.left
+            else:
+                curr = stack.pop()
+                k -= 1
+                if k == 0:
+                    return curr.val
+                curr = curr.right
