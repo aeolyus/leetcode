5fde45bb9b61024f1d99c41a8199ea6aaf6d3a11
Richard Huang <richardhuang.huang@gmail.com>
2024-01-21 21:02:29 -0800

    [0104][python] maximum depth of binary tree

---
+++ python/0104/main.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+        stack = [(root, 1)]
+        result = 0
+        while stack:
+            node, depth = stack.pop()
+            result = max(result, depth)
+            if node.left:
+                stack.append((node.left, depth + 1))
+            if node.right:
+                stack.append((node.right, depth + 1))
+        return result
---
+++ python/0104/test.py
@@ -0,0 +1,46 @@
+import unittest
+from main import Solution, TreeNode
+
+
+class Test0104(unittest.TestCase):
+    def test(self):
+        testcases = [
+            [[3, 9, 20, None, None, 15, 7], 3],
+            [[1, None, 2], 2],
+        ]
+
+        for i in testcases:
+            lst, expected = i
+            root = build_tree(lst)
+            s = Solution()
+            actual = s.maxDepth(root)
+            self.assertEqual(expected, actual)
+
+
+def build_tree(lst) -> TreeNode:
+    lookup = {}
+    root = None
+    offset = 0
+    for i, val in enumerate(lst):
+        if val is None:
+            offset += 1
+            continue
+        node = None
+        if root is None:
+            node = TreeNode(val)
+            root = node
+        else:
+            node = lookup[i]
+        left = 2*(i - offset) + 1
+        right = 2*(i - offset) + 2
+        if left < len(lst) and lst[left] is not None:
+            node.left = TreeNode(lst[left])
+            lookup[left] = node.left
+        if right < len(lst) and lst[right] is not None:
+            node.right = TreeNode(lst[right])
+            lookup[right] = node.right
+    return root
+
+
+if __name__ == '__main__':
+    unittest.main()
