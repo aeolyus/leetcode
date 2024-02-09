0f90e88483acf896af9fd14e1c1c8f5bc02d3ac1
Richard Huang <richardhuang.huang@gmail.com>
2024-02-08 20:11:51 -0800

    [0236][python] recursive solution

--- python/0236/main.py
+++ python/0236/main.py
@@ -1,6 +1,3 @@
-from typing import List
-
-
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, x):
@@ -16,24 +13,10 @@ class Solution:
             p: TreeNode,
             q: TreeNode,
     ) -> TreeNode:
-        def get_node_path(root: TreeNode, dest: TreeNode) -> List[TreeNode]:
-            queue = [(root, [])]
-            while queue:
-                node, path = queue.pop(0)
-                if node is None:
-                    continue
-                if node.val == dest.val:
-                    return path + [node]
-                queue.append((node.left, path + [node]))
-                queue.append((node.right, path + [node]))
-        p_path = get_node_path(root, p)
-        q_path = get_node_path(root, q)
-        lca = None
-        i = 0
-        while i < len(p_path) and i < len(q_path):
-            if p_path[i].val == q_path[i].val:
-                lca = p_path[i]
-            else:
-                break
-            i += 1
-        return lca
+        if root is None or root.val == p.val or root.val == q.val:
+            return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        if left and right:
+            return root
+        return left or right
