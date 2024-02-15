de980bb72108147f092a4cc3371cf42cbd13d937
Richard Huang <richardhuang.huang@gmail.com>
2024-02-14 19:50:16 -0800

    [0337][python] speed up with dp

--- python/0337/main.py
+++ python/0337/main.py
@@ -11,14 +11,20 @@ class TreeNode:
 
 class Solution:
     def rob(self, root: Optional[TreeNode]) -> int:
+        return self.helper(root, {})
+
+    def helper(self, root, dp):
         if root is None:
             return 0
-        i = self.rob(root.left) + self.rob(root.right)
+        if root in dp:
+            return dp[root]
+        i = self.helper(root.left, dp) + self.helper(root.right, dp)
         j = root.val
         if root.left:
-            j += self.rob(root.left.left)
-            j += self.rob(root.left.right)
+            j += self.helper(root.left.left, dp)
+            j += self.helper(root.left.right, dp)
         if root.right:
-            j += self.rob(root.right.left)
-            j += self.rob(root.right.right)
-        return max(i, j)
+            j += self.helper(root.right.left, dp)
+            j += self.helper(root.right.right, dp)
+        dp[root] = max(i, j)
+        return dp[root]
