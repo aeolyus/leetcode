cf7c4cc64689c33a107b91a94ed9f03f6907cdeb
Richard Huang <richardhuang.huang@gmail.com>
2024-02-15 20:57:43 -0800

    [0128][python] iterate through set instead of list

--- python/0128/main.py
+++ python/0128/main.py
@@ -5,13 +5,10 @@ class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         num_set = set(nums)
         result = 0
-        for n in nums:
-            run = 0
-            if n - 1 in num_set:
-                continue
-            else:
-                while n in num_set:
+        for n in num_set:
+            if (n - 1) not in num_set:
+                run = 1
+                while (n + run) in num_set:
                     run += 1
-                    n += 1
                 result = max(result, run)
         return result
